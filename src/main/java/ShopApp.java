e7eb222948b735d602a6f5eb138c81f7e957aa9a
Kacper <kacperstaszek1995@gmail.com>
2019-01-12 09:30:33 +0100

    Create connection

--- src/main/java/ShopApp.java
+++ src/main/java/ShopApp.java
@@ -1,7 +1,17 @@
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 
 public class ShopApp {
     public static void main(String[] args) {
-        new MongoClient()
+        MongoClient mongoClient = new MongoClient(
+                new ServerAddress("localhost", 27017),
+                MongoCredential.createCredential("javakrk", "library", "javakrk".toCharArray()),
+                MongoClientOptions.builder().build()
+        );
+
+        mongoClient.close();
     }
+
 }
